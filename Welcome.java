4aaff9b4819a0dd9c4856b381769401724771242
ChVinicius <charlesviniciussousaaraujo@gmail.com>
2024-06-26 19:59:03 -0300

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,15 @@
-public class Welcome{
-    public static void main(String[] args){
-System.out.println("Olá, Mundo!");
+import java.util.Scanner;
+
+public class Welcome {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá mundo " + nome);
+
+
+        }
+
     }
-}
